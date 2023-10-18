6720137f18216d1b36afcb113d3e11a4ac21e743
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-18 22:42:16 +0200

    labdonerafael.js

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,59 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    // const newDirectors = [];
+    // moviesArray.forEach(function(each)  {
+    //     newDirectors.push(each.director)
+    // });
+    // return newDirectors 
+    const directors = moviesArray.map(function(eachDirector){
+        return eachDirector.director
+    })
+    console.log(directors)
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let movie = 0;
+    let dramaMovies = [];
+
+for (let i = 0; i < moviesArray.length; i++){
+    //return howManyMovies.length;
+    if(moviesArray[i].director === 'Steven Spielberg' && moviesArray[i].genre.includes('Drama')){
+        dramaMovies.push(moviesArray[i]);
+    }
+
+}
+
+return dramaMovies.length
+}
+
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    
+    // console.log('all movies', moviesArray)
+    // const scoreMovie = moviesArray.filter((eachMovie){
+    //     return eachMovie.scoreMovie
+    // })
+    // const scored = moviesArray.map((eachMovie){
+    //     return eachMovie.score
+    // })
+
+    // const scoreMovie = moviesArray.filter((eachMovie)=>{
+    //     if(eachMovie.score){
+    //         return true
+    //     }
+    // })
+    // const movieScore = moviesArray.map((eachMovie) =>{
+    //     if(eachMovie.)
+    // })
+
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
